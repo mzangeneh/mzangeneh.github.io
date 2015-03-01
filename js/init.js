6fd89a2859db90eafb9935b5f83d9300019e745b
zangeneh <mojtaba.zangeneh@gmail.com>
2015-03-01 23:30:34 +0330

    first commit

---
+++ js/init.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "1 May 2015 11:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
